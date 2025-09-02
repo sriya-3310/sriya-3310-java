1b8b9e65ef7f2ddbc888984e22f41b1afa219670
sriya-3310 <reddysriya525@gmail.com>
2025-09-02 16:50:25 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,11 @@
+package javaprog4;
+
+public class Person {
+String Name;
+int Age;
+static String Nationality="Indian";
+public static void main(String[] args) {
+	System.out.println(Nationality);
+	System.out.println(Person.Nationality);
+}
+}
