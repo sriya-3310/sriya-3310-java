45ffe976bd89a7127d5fb1540036be92051a99ab
sriya-3310 <reddysriya525@gmail.com>
2025-09-02 17:01:01 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,14 @@
+package day2prog2;
+
+public class Demo {
+	public static void main(String[]args) {
+		System.out.println(35+45+"value is");
+		System.out.println(10);
+		System.out.println(50+35+"value is");
+		System.out.println(90+100+"value is "+90+100);
+		System.out.println(2+3+  "value is "+(2+3));
+		
+		
+	}
+
+}
