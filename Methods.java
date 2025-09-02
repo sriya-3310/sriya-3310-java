fcfd19bf48fcdf811c39af4c86006b0ab8e49e49
sriya-3310 <reddysriya525@gmail.com>
2025-09-02 17:12:11 +0530

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,11 @@
+package javaprog4;
+
+public class Methods {
+  static void Method(int val )
+   {
+	   System.out.println("no parameter and no return value");
+   }
+   public static void main(String[] args) {
+	   Method(10);
+   }
+}
